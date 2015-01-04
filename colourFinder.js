3bf2be427f90cdf3a72b58a3ff5234ed9d353ae2
Drew Miller <drew@onpatrol.co>
2015-01-04 12:42:49 +0000

    Add a new function to search a file and return the lines that contain colours

--- colourFinder.js
+++ colourFinder.js
@@ -3,16 +3,29 @@
 
 var glob = require('glob');
 var fs = require('fs');
+var search = require('./lib/search');
+
+var filenames = [];
 
 glob('sass/**/*.scss', function (err, files) {
     if (err) {
         return console.log(err);
     }
 
-    console.log(files);
-
     // open each file and look for colour hex values
+    for (var file in files) {
+        var openFile = files[file];
 
+        fs.readFile(openFile, 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+
+            filenames.push({
+                filename: openFile,
+                lines: search.searchFile(data)
+            });
+        });
+    }
 });
 
-// sort all the colours 
--- test/searchTest.js
+++ test/searchTest.js
@@ -4,6 +4,62 @@ var search = require('../lib/search');
 
 describe('Searching for colour hex values', function () {
 
+    describe('searchFile function', function () {
+        it('returns an array', function () {
+            var result = search.searchFile('');
+
+            expect(result).to.be.instanceOf(Array);
+        });
+
+        it('returns an object for every line that has a colour found', function () {
+            var result = search.searchFile('#f00');
+
+            expect(result[0]).to.be.instanceOf(Object);
+        });
+
+        describe('line object', function () {
+            var match;
+
+            beforeEach(function () {
+                match = search.searchFile("#f00 #0f0 #00f")[0];
+            });
+
+            it('has a line number that is an integer', function () {
+                expect(match).to.have.a.property('line');
+                expect(match.line).to.be.a('number');
+            });
+
+            it('has a list of colours that is an array', function () {
+                expect(match).to.have.a.property('colours');
+                expect(match.colours).to.be.instanceOf(Array);
+            });
+        });
+
+        describe('search results', function () {
+            it('finds all the lines with colours', function () {
+                var results = search.searchFile("#f00 #00ff00\n#0f0");
+
+                expect(results.length).to.equal(2);
+                expect(results[0].line).to.equal(1);
+                expect(results[0].colours.length).to.equal(2);
+                expect(results[1].line).to.equal(2);
+                expect(results[1].colours.length).to.equal(1);
+            });
+
+            it('should not return a line that has no colours', function () {
+                var results = search.searchFile("#\n#00ff00");
+
+                expect(results.length).to.equal(1);
+            });
+
+            it('should not return any results on an empty file', function () {
+                var results = search.searchFile("");
+
+                expect(results.length).to.equal(0);
+            });
+        });
+    });
+
     describe('findColours function', function () {
         it('returns an array', function () {
             var result = search.findColours('');
