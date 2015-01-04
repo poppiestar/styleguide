349725bd30ccf319ec57812329284619a1457fe1
Drew Miller <drew@onpatrol.co>
2015-01-04 12:07:21 +0000

    Improve tests

--- test/searchTest.js
+++ test/searchTest.js
@@ -24,12 +24,14 @@ describe('Searching for colour hex values', function () {
                 match = search.findColours("#f00 #0f0 #00f")[0];
             });
 
-            it('has a value', function () {
+            it('has a value that is a string', function () {
                 expect(match).to.have.a.property('value');
+                expect(match.value).to.be.a('string');
             });
 
-            it('has a position', function () {
+            it('has a position that is a number', function () {
                 expect(match).to.have.a.property('position');
+                expect(match.position).to.be.a('number');
             });
         });
 
@@ -38,40 +40,32 @@ describe('Searching for colour hex values', function () {
                 var results = search.findColours("#f00 #00ff00 #0f0 #f00");
 
                 expect(results.length).to.equal(4);
-                expect(results[0].value).to.equal('#f00');
-                expect(results[0].position).to.equal(0);
-                expect(results[1].value).to.equal('#00ff00');
-                expect(results[1].position).to.equal(5);
-                expect(results[2].value).to.equal('#0f0');
-                expect(results[2].position).to.equal(13);
-                expect(results[3].value).to.equal('#f00');
-                expect(results[3].position).to.equal(18);
+                expect(results[0]).to.deep.equal({ value: '#f00', position: 0 });
+                expect(results[1]).to.deep.equal({ value: '#00ff00', position: 5 });
+                expect(results[2]).to.deep.equal({ value: '#0f0', position: 13 });
+                expect(results[3]).to.deep.equal({ value: '#f00', position: 18 });
             });
 
             it('finds three character hex values', function () {
                 var results = search.findColours("#f00");
 
                 expect(results.length).to.equal(1);
-                expect(results[0].value).to.equal('#f00');
-                expect(results[0].position).to.equal(0);
+                expect(results[0]).to.deep.equal({ value: '#f00', position: 0 });
             });
 
             it('six character hex values', function () {
                 var results = search.findColours("#ff0000");
 
                 expect(results.length).to.equal(1);
-                expect(results[0].value).to.equal('#ff0000');
-                expect(results[0].position).to.equal(0);
+                expect(results[0]).to.deep.equal({ value: '#ff0000', position: 0 });
             });
 
             it('finds a mix of hex values', function () {
                 var results = search.findColours("#ff0000 #00f");
 
                 expect(results.length).to.equal(2);
-                expect(results[0].value).to.equal('#ff0000');
-                expect(results[0].position).to.equal(0);
-                expect(results[1].value).to.equal('#00f');
-                expect(results[1].position).to.equal(8);
+                expect(results[0]).to.deep.equal({ value: '#ff0000', position: 0 });
+                expect(results[1]).to.deep.equal({ value: '#00f', position: 8 });
             });
 
             it('should not return an invalid hex value', function () {
